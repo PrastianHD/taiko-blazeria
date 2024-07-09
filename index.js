efe6fe5d8f80f840ab96ec63a3e00ecadc8f68ac
PrastianHD <48909279+PrastianHD@users.noreply.github.com>
2024-07-09 11:49:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,51 +31,72 @@ async function main() {
     ];
 
     // WETH contract address
-    const addressWETH = '0xA51894664A773981C6C112C43ce576f315d5b1B6';
+    const addressWETH = '0xf531B8F309Be94191af87605CfBf600D71C2cFe0';
     const contractWETH = new ethers.Contract(addressWETH, abiWETH, wallet);
 
     function delay(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
+    function randomDelay() {
+        const minDelay = 1; // Minimum delay in seconds
+        const maxDelay = 60; // Maximum delay in seconds
+        const delay = Math.floor(Math.random() * (maxDelay - minDelay + 1) + minDelay) * 1000; // Convert to milliseconds
+        return delay;
+    }
+    
     function generateRandomAmount() {
         const amounts = Math.floor(Math.random() * 90) + 10;
-        const amountInETH = "0.00000" + amounts.toString().padStart(2, '0');
+        const amountInETH = "0.0000000" + amounts.toString().padStart(2, '0');
         return ethers.parseUnits(amountInETH, 'ether'); 
     }
 
-    const transactionCount = 2500; 
+    function generateRandomAmountString() {
+        const amounts = Math.floor(Math.random() * 90) + 10;
+        const amountInETH = "0.0000000" + amounts.toString().padStart(2, '0');
+        return amountInETH;
+    }
+    
+    const transactionCount = 150; 
 
     async function depositTransactions(transactionCount) {
         let successCount = 0;
+        let totalAttempts = 0;
 
-        for (let i = 0; i < transactionCount; i++) {
+        while (successCount < transactionCount) {
             let retryCount = 0;
             const maxRetries = 3;
-
+    
             while (retryCount < maxRetries) {
                 try {
-                    const deposit = await contractWETH.deposit({ value: generateRandomAmount() });
-                    log('SUCCESS', `Deposit sent with hash: ${deposit.hash}`);
-                    const withdraw = await contractWETH.withdraw(ethers.parseEther("0.000005"));
-                    log('SUCCESS', `Withdraw sent with hash: ${withdraw.hash}`);
-
-                    successCount++;
+                    if (totalAttempts % 2 === 0) {
+                        const deposit = await contractWETH.deposit({ value: generateRandomAmount() });
+                        successCount++;
+                        log('SUCCESS', `Transaction ${successCount} with hash: ${deposit.hash}`);
+                        await delay(randomDelay());
+                    } else {
+                        const withdraw = await contractWETH.withdraw(ethers.parseEther(generateRandomAmountString()));
+                        successCount++;
+                        log('SUCCESS', `Transaction ${successCount} with hash: ${withdraw.hash}`);
+                        await delay(randomDelay());
+                    }
                     break;
 
                 } catch (error) {
                     retryCount++;
-                    log('ERROR', `Error sending transaction, retry ${retryCount}: ${error.message}`);
+                    let errorMessage = error.message;
+                    if (error.code === 'INSUFFICIENT_FUNDS') {
+                        errorMessage = 'INSUFFICIENT_FUNDS';
+                    }
+                    if (error.code === 'SERVER_ERROR') {
+                        errorMessage = 'Service Temporarily Unavailable';
+                    }
+                    log('ERROR', `Error sending transaction, retry ${retryCount}: ${errorMessage}`);
                     await delay(1000); 
                 }
             }
 
-            if ((i + 1) % 51 === 0) {
-                log('DEBUG', `Completed transactions for this day...`);
-                log('DEBUG', `Transaction Next Day...`);
-                await delay(24 * 60 * 60 * 1000); 
-            }
-
+            totalAttempts++;
             await delay(100); 
         }
     }
